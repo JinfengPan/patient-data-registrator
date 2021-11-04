616009f3cc2687b39b42cc5ff4b3a890f3eec3eb
Jinfeng <jinfeng.pan@outlook.com>
2021-11-04 22:14:27 +0000

    add fakePatientDataservice

---
+++ src/services/fakePatientDataService.js
@@ -0,0 +1,37 @@
+const patients = [
+  {
+    _id: "",
+    name: "张三",
+    age: "",
+    gender: "",
+    hometown: "",
+    onsetTime: "",
+    courseOfDisease: "",
+
+    hasHeadache: true,
+    hasConvulsion: true,
+    hasConsciousnessDisorder: true,
+    hasHemiplegia: true,
+    hasIncontinence: true,
+
+    headacheDesc: "",
+    convulsionDesc: "",
+    ConsciousnessDisorderDesc: "",
+    HemiplegiaDesc: "",
+    IncontinenceDesc: "",
+
+    hasUseDrug: true,
+    drugType: "",
+    specificDrug: "",
+
+    hasSurgery: true,
+    surgeryMethod: "",
+
+    complicationHeadache: true,
+    complicationInfection: true,
+    complicationConvulsion: true,
+    complicationSecondSurgery: true,
+
+    followUpTime: "",
+  },
+];
