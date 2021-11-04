85ce3957e09b68d861b1b9136b5074df758f2677
Jinfeng <jinfeng.pan@outlook.com>
2021-11-04 21:39:04 +0000

    update app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>慢性硬膜下血肿患者资料登记系统</h1>
+      <button>输入新病例</button>
+      <button>导出数据</button>
+      <button>展示全部病例</button>
+      <button>退出</button>
+    </div>
+  );
+}
+
+export default App;
