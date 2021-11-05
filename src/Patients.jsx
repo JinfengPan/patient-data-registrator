aaef3c7b85c93fe13867ce3186da88c2b3a83f83
Patrick Pan <S6P4@eurofins.com>
2021-11-05 11:56:49 +0000

    update patients

--- src/Patients.jsx
+++ src/Patients.jsx
@@ -1,16 +1,44 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { getPatients } from "./services/fakePatientDataService";
 
 class Patients extends React.Component {
+  state = {
+    patients: getPatients(),
+  };
 
-    state = {
-        patients: getPatients()
-    };
-    
-    render() { 
-        return <div></div>;
-    }
+  render() {
+    const { length: count } = this.state.patients;
+
+    if (count === 0) return <p>目前尚没有患者资料。</p>;
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.patients.map((patient) => (
+              <tr>
+                <td>{patient.name}</td>
+                <td>{patient.age}</td>
+                <td>{patient.gender}</td>
+                <td>{patient.hometown}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
- 
-export default Patients;
 
+export default Patients;
