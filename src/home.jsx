56a899b258bdc8aaf0f70d454a6e8a00aa037010
Jinfeng <jinfeng.pan@outlook.com>
2021-11-05 21:24:11 +0000

    add links

--- src/home.jsx
+++ src/home.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <>
       <h1>慢性硬膜下血肿患者资料登记系统</h1>
-      <button className="btn btn-primary">输入新病例</button>
+      <Link to="/add" className="btn btn-primary">
+        输入新病例
+      </Link>
       <button className="btn btn-primary">导出数据</button>
-      <button className="btn btn-primary">展示全部病例</button>
+      <Link to="/patients" className="btn btn-primary">
+        展示全部病例
+      </Link>
       <button className="btn btn-primary">退出</button>
     </>
   );
